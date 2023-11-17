199e3fed47c30061e865c851480fd120d5bd7356
01subhashree/ <ssubhashree094@gmail.com>
2023-11-17 20:43:54 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,98 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useState } from "react";
+import styles from "./page.module.css";
+
+const OPENAI_API_KEY = "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";
 
 export default function Home() {
+  const [data, setData] = useState("");
+  const [inputData, setInputData] = useState("");
+  const [historyData, setHistoryData] = useState("");
+
+  const handleClick = (text) => {
+    setInputData(text);
+  };
+  const clickHandler = async () => {
+    console.log(inputData);
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: inputData,
+          },
+        ],
+        max_tokens: 100,
+      }),
+    };
+    try {
+      const res = await fetch(
+        " https://api.openai.com/v1/chat/completions",
+        options
+      );
+      const fetchData = await res.json();
+      const ouput = fetchData.choices[0].message.content;
+      setData(ouput);
+      console.log(fetchData);
+      setHistoryData([...historyData, inputData]);
+      setInputData("");
+    } catch {
+      (err) => console.log(err);
+    }
+  };
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+      <div className={styles.leftSidebar}>
+        <div className={styles.buttonDiv}>
+          <img
+            src="https://tse3.mm.bing.net/th?id=OIP.IxjleaQdJ-PbHB9eekqqUQHaHb&pid=Api&P=0&h=180"
+            width={40}
+            style={{ borderRadius: "2rem" }}
+          />
+          <button className={styles.button} onClick={clickHandler}>
+            New Chat
+          </button>
+        </div>
+        <div className={styles.history_Div}>
+          {historyData &&
+            historyData.map((ele, index) => (
+              <p key={index} onClick={() => handleClick(ele)}>
+                {ele}
+              </p>
+            ))}
+        </div>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <p>Made by subhashree</p>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+      <div className={styles.rightSidebar}>
+        <div className={styles.navbar}>
+          <h2>ChatGPT</h2>
+        </div>
+        <p className={styles.outputDiv}>{data}</p>
+        <div className={styles.buttom_section}>
+          <div className={styles.input_div}>
+            <input
+              className={styles.inputBox}
+              placeholder="Message ChatGPT ..."
+              value={inputData}
+              onChange={(e) => setInputData(e.target.value)}
+            />
+            <button className={styles.submit} onClick={clickHandler}>
+              Submit
+            </button>
+          </div>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            ChatGPT can make mistakes. Consider checking important information.
           </p>
-        </a>
+        </div>
       </div>
     </main>
-  )
+  );
 }
