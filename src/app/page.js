b104501d346593f22ff90989b61c19d852cbbf1a
01subhashree/ <ssubhashree094@gmail.com>
2023-11-18 19:05:02 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,63 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useState } from "react";
+import styles from "./page.module.css";
+
+const OPENAI_API_KEY = "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";
 
 export default function Home() {
+  const [inputText, setInputText] = useState("");
+  const [fetchImages, setFetchImages] = useState([]);
+
+  const getImages = async () => {
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: inputText,
+        n: 4,
+        size: "1024x1024",
+      }),
+    };
+    try {
+      const res = await fetch(
+        "https://api.openai.com/v1/images/generations",
+        options
+      );
+      const data = await res.json();
+      setFetchImages(data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log("fetchImages", fetchImages);
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <h1>AI Image Generator</h1>
+      <div className={styles.contentDiv}>
+        {fetchImages &&
+          fetchImages.map((ele, index) => (
+            <img key={index} src={ele.url} className={styles.imageContent} />
+          ))}
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className={styles.buttomDiv}>
+        <input
+          className={styles.inputBOx}
+          placeholder="Enter text ..."
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <img
+          onClick={getImages}
+          src="https://icon-library.com/images/submit-icon-png/submit-icon-png-13.jpg"
+          alt="submit button"
+          width={25}
         />
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
